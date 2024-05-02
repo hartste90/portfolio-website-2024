67c16575107cfd012352f42637c332ebf27d25c6
Steve Hart <hartste90@gmail.com>
2024-05-02 14:40:29 -0700

    fix missing import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 /// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import Hero from "./components/hero";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
