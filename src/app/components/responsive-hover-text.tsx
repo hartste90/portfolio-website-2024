cfaf13a553fb5b990d9aa8341de88a06dd2b623f
Steve Hart <hartste90@gmail.com>
2024-06-20 17:10:02 -0700

    bugfix: key error in hero

--- src/app/components/responsive-hover-text.tsx
+++ src/app/components/responsive-hover-text.tsx
@@ -3,8 +3,8 @@ export function responsiveHoverText(text: string)
 {
   let heroMainTextArray: JSX.Element[] = [];
   text.split(" ").map((word) => {
-    heroMainTextArray.push(<a className='lift-on-hover inline-block text-3xl sm:text-6xl'>{word}</a>);      
-    heroMainTextArray.push(<span className='lift-on-hover inline-block text text-3xl sm:text-6xl'>&nbsp;</span>)
+    heroMainTextArray.push(<a key={word} className='lift-on-hover inline-block text-3xl sm:text-6xl'>{word}</a>);      
+    heroMainTextArray.push(<span key={word+"space"} className='lift-on-hover inline-block text text-3xl sm:text-6xl'>&nbsp;</span>);
   });
   return heroMainTextArray;
 }
